1f99edb883bee999d66c4f98c13dc7abba271d8a
Ibournakas <ibournakas@gmail.com>
2023-03-23 01:35:25 +0200

    Login and main

---
+++ code/app/src/main/java/com/example/papapp/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.papapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+public class HomeActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+
+
+}}
---
+++ code/app/src/main/java/com/example/papapp/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.papapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private Button mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Initialize views
+        mEmailEditText = findViewById(R.id.editText1);
+        mPasswordEditText = findViewById(R.id.editText2);
+        mLoginButton = findViewById(R.id.button);
+
+        mPasswordEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    mLoginButton.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
+        // Set click listener for login button
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get user input
+                String email = mEmailEditText.getText().toString().trim();
+                String password = mPasswordEditText.getText().toString().trim();
+
+                // Validate user input
+                if (TextUtils.isEmpty(email)) {
+                    mEmailEditText.setError("Email is required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPasswordEditText.setError("Password is required");
+                    return;
+                }
+
+                // Authenticate user
+                if (email.equals("admin") && password.equals("admin")) {
+                    // Login successful
+                    Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    // Start main activity
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Login failed
+                    Toast.makeText(MainActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
+
