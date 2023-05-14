69a52a2df3ad5f2a51a65decd1c4387040a77f10
Ibournakas <ibournakas@gmail.com>
2023-05-14 17:24:43 +0300

    layout for the cafe menu

--- code/app/src/main/java/com/example/papapp/CafeMenu.java
+++ code/app/src/main/java/com/example/papapp/CafeMenu.java
@@ -1,14 +1,31 @@
 package com.example.papapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.papapp.Announcement_Services.Announcements;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
 public class CafeMenu  extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
+    final Calendar myCalendar= Calendar.getInstance();
+    private EditText dateText;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +35,36 @@ public class CafeMenu  extends AppCompatActivity {
         } else {
             setTheme(R.style.Theme_Light);
         }
-        setContentView(R.layout.cafeteria);
+        setContentView(R.layout.cafemenu);
+
+        DatePickerDialog.OnDateSetListener datePickerListener = (view, year, month, dayOfMonth) -> {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH,month);
+            myCalendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+            updateDateLabel();
+        };
+
+        dateText = findViewById(R.id.date_picker);
+        dateText.setOnClickListener(v -> new DatePickerDialog(CafeMenu.this,datePickerListener,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show());
+
+        Spinner spinner = findViewById(R.id.menu_spinner);
+        String[] options = {"Day", "Week"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, options);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0); // Set "Day" as the default selection
+
+
 
     }
+    private void updateDateLabel(){
+        if(dateText.getError() != null){
+            dateText.setError(null);
+        }
+        String myFormat="dd-MM-yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.getDefault());
+        dateText.setText(dateFormat.format(myCalendar.getTime()));
+    }
+
 }
+
+
--- code/app/src/main/java/com/example/papapp/MenuFetcher.java
+++ code/app/src/main/java/com/example/papapp/MenuFetcher.java
@@ -1,4 +1,39 @@
 package com.example.papapp;
 
-public class MenuFetcher {
+import android.os.AsyncTask;
+import android.util.Log;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class MenuFetcher extends AsyncTask<Void, Void, String> {
+
+    private static final String TAG = "MenuFetcher";
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        String menuUrl = "https://www.upatras.gr/wp-content/uploads/2023/04/ΠΡΟΓΡΑΜΜΑ-ΣΙΤΙΣΗΣ-ΜΑΪΟΣ-2023_compressed.pdf";
+        try {
+            URL url = new URL(menuUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                InputStream in = urlConnection.getInputStream();
+                Scanner scanner = new Scanner(in);
+                scanner.useDelimiter("\\A");
+                boolean hasInput = scanner.hasNext();
+                if (hasInput) {
+                    return scanner.next();
+                } else {
+                    return null;
+                }
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error fetching menu data", e);
+            return null;
+        }
+    }
 }
