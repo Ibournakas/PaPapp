a56bf51cb6dca1d26abee62d827f1c8fecbb3efe
Ibournakas <ibournakas@gmail.com>
2023-05-15 19:21:48 +0300

    Cafe menu initial

--- code/app/src/main/java/com/example/papapp/MenuFetcher.java
+++ code/app/src/main/java/com/example/papapp/MenuFetcher.java
@@ -4,25 +4,40 @@ import android.os.AsyncTask;
 import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
+
 import java.net.URL;
-import java.util.Scanner;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 
 
-public class MenuFetcher extends AsyncTask<String, Void, String[][][]> {
+
+public class MenuFetcher extends AsyncTask<String, Void, String[][][][]> {
     private String monthName;
     private static final String TAG = "MenuFetcher";
+    private PDFont greekFont;
 
 
     public MenuFetcher(String monthName) {
         this.monthName = monthName;
+
+        // Load the Greek font
+        try {
+            InputStream fontInputStream = getClass().getResourceAsStream("/fonts/ArialGr.ttf");
+            PDDocument tempDoc = new PDDocument();
+            greekFont = PDType0Font.load(tempDoc, fontInputStream);
+            tempDoc.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading Greek font", e);
+        }
     }
 
     @Override
-    protected String[][][] doInBackground(String... strings) {
-        String[][][] menuData = null;
+    protected String[][][][] doInBackground(String... strings) {
+        String[][][][] menuData = null;
+        // Initialize the PDDocument object
+        PDDocument document = null;
         // Map each month name to its Greek equivalent
         String greekMonth;
         switch (monthName) {
@@ -70,16 +85,16 @@ public class MenuFetcher extends AsyncTask<String, Void, String[][][]> {
         String menuUrl = "https://www.upatras.gr/wp-content/uploads/2023/04/ΠΡΟΓΡΑΜΜΑ-ΣΙΤΙΣΗΣ-" + greekMonth.toUpperCase() + "-2023_compressed.pdf";
         try {
             InputStream inputStream = new URL(menuUrl).openStream();
-            PDDocument document = PDDocument.load(inputStream);
-            PDFTextStripper stripper = new GreekPDFTextStripper();
-            String text = stripper.getText(document);
+             document = PDDocument.load(inputStream);
+            GreekPDFTextStripper stripper = new GreekPDFTextStripper(greekFont);
+            String text =stripper.getText(document);
 
             // Split the text into pages
             String[] pages = text.split("\f");
 
             // Initialize the menuData array
             int numWeeks = pages.length / 2;
-            menuData = new String[numWeeks][2][];
+            menuData = new String[numWeeks][2][][];
 
             // Extract the menu data for each week
             int pageOffset = 0;
@@ -87,11 +102,14 @@ public class MenuFetcher extends AsyncTask<String, Void, String[][][]> {
                 // Extract the launch and dinner menus for the current week
                 String[] launchMenu = extractMenuData(pages[pageOffset]);
                 String[] dinnerMenu = extractMenuData(pages[pageOffset + 1]);
+                // Parse the launch and dinner menus for the current week
                 String[][] launchMeals = parseMenu(launchMenu);
                 String[][] dinnerMeals = parseMenu(dinnerMenu);
+
                 // Store the meals in the menuData array
                 menuData[i][0] = launchMeals;
                 menuData[i][1] = dinnerMeals;
+
                 // Increment the page offset to move to the next week
                 pageOffset += 2;
             }
@@ -108,6 +126,31 @@ public class MenuFetcher extends AsyncTask<String, Void, String[][][]> {
         }
         return  menuData;
     }
+    private String[] extractMenuData(String page) {
+        // Extract the menu items from the page
+        String[] menuItems = page.trim().split("\n");
+        // Remove empty lines and leading/trailing whitespaces
+        for (int i = 0; i < menuItems.length; i++) {
+            menuItems[i] = menuItems[i].trim();
+        }
+        return menuItems;
+    }
+
+    private String[][] parseMenu(String[] menuItems) {
+        String[][] meals = new String[menuItems.length][];
+        for (int i = 0; i < menuItems.length; i++) {
+            String[] mealItems = menuItems[i].split(",");
+            for (int j = 0; j < mealItems.length; j++) {
+                mealItems[j] = mealItems[j].trim();
+            }
+            meals[i] = mealItems;
+        }
+        return meals;
+    }
+
+
+
+
 }
 
 
